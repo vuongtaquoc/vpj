a6e4371357867aecdbca3d17074d47980e26bed3
Vuong Ta Quoc <tqv.itvn@gmail.com>
2019-07-24 21:34:44 +0700

    change request

--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,56 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-	var _width = $(window).width();
+  var _width = $(window).width();
 
-	$('.navbar a, footer a[href="#myPage"]').on('click', function(event) {
-		if (this.hash !== '') {
+  $(".navbar a, footer a[href='#myPage']").on('click', function (event) {
 
-			event.preventDefault();
+    if (this.hash !== "") {
 
-			var hash = this.hash;
+      event.preventDefault();
 
-			$('html, body').animate({
-				scrollTop: $(hash).offset().top
-			}, 900, function() {
-				window.location.hash = hash;
-			});
-		}
-	});
+      var hash = this.hash;
 
-	$(window).scroll(function() {
-		$('.slideanim').each(function() {
-			var pos = $(this).offset().top;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function () {
 
-			var winTop = $(window).scrollTop();
-			if (pos < winTop + 600) {
-				$(this).addClass('slide');
-			}
-		});
-	});
-	if (_width > 1366) {
-		var _height = $('.timeline .timeline-img').height();
-		$('.timeline .timeline-text').css({
-			'margin-top': _height / 3
-		})
-	}
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
+  $(window).scroll(function () {
+    $(".slideanim").each(function () {
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+      if (pos < winTop + 600) {
+        $(this).addClass("slide");
+      }
+    });
+  });
+  if (_width > 1366) {
+    var _height = $(".timeline").height();
+    var _height1 = $(".timeline .col-sm-4").height();
+    var _h3 = _height - _height1;
+    $(".timeline .col-sm-4").css({
+      'margin-top': _h3 / 2
+    })
+  }
+  if (_width > 767) {
+    var _textWidth = $(".judging ul li:nth-child(5) .span-img").width();
+
+    $(".judging ul li:nth-child(5) .span-text,.judging ul li:nth-child(4) .span-text,.judging ul li:nth-child(6) .span-text").css({
+      right: _textWidth
+    });
+    $(".judging ul li:nth-child(1) .span-text,.judging ul li:nth-child(2) .span-text,.judging ul li:nth-child(3) .span-text").css({
+      left: _textWidth
+    });
+
+  }
+  if (_width > 1024) {
+    var _htimeline = $("#timeline").height();
+
+    $(".bg-bottom img").height(_htimeline * 2);
+  }
 })
